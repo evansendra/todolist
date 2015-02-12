ae10d9f64b6427a47da44dfdfb63bb54586eab63
Evan Sendra <code@evansendra.com>
2015-02-11 23:35:41 -0600

    model todos created without autofilled timestamps

---
+++ app/controllers/todos.rb
@@ -0,0 +1,30 @@
+Todolist::App.controllers :todos do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :index do
+		@todos = Todo.all(:order => 'created_at desc')
+		render 'todos/index'
+  end
+
+  get :show do
+  end
+
+end
---
+++ db/migrate/001_create_todos.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+	Sequel::Model.plugin :timestamps
+
+  up do
+    create_table :todos do
+      primary_key :id
+      String :title
+      String :author
+      Boolean :is_completed
+			DateTime :created_at
+			DateTime :updated_at
+    end
+  end
+
+  down do
+    drop_table :todos
+  end
+end
