1f87254c61362b8a5d722e61dc0fe730fd09aa70
Evan Sendra <code@evansendra.com>
2015-02-16 01:44:27 -0600

    change to simplified db config to see if heroku still deploys correctly

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,6 @@ Sequel::Model.plugin(:validation_helpers)
 Sequel::Model.raise_on_save_failure = true # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.postgres('todonelist', :user => 'rkkn', :password => 'admin', :host => 'localhost', :port => 5432)
-  when :production  then Sequel.postgres('d5uf67kfnh2cur', :user=>'kmcdsfluyjnflu', :password=>'ZinURed_EdW6XSAJxZLCWGMex7', :host => 'ec2-23-23-210-37.compute-1.amazonaws.com', :port => 5432)
+  when :production  then Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
   when :test        then Sequel.postgres('todonelist', :user => 'rkkn', :password => 'admin', :host => 'localhost', :port => 5432)
 end
