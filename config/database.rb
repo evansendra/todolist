36a5f394627647e36c083f026b9b57ca7194be90
Evan Sendra <code@evansendra.com>
2015-02-16 00:21:31 -0600

    create pg database auth for heroku

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,6 @@ Sequel::Model.plugin(:validation_helpers)
 Sequel::Model.raise_on_save_failure = true # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("sqlite:///" + Padrino.root('db', "todolist_development.db"), :loggers => [logger])
-  when :production  then Sequel.postgres('todonelist', :user=>'admin', :password=>'rkkncat2015')
+  when :production  then Sequel.postgres(:host => 'ec2-23-23-210-37.compute-1.amazonaws.com', 'd5uf67kfnh2cur', :user=>'kmcdsfluyjnflu', :password=>'ZinURed_EdW6XSAJxZLCWGMex7', :port => 5432)
   when :test        then Sequel.connect("sqlite:///" + Padrino.root('db', "todolist_test.db"),        :loggers => [logger])
 end
